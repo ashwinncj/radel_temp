22976c504da55885c6ed8d478de91c0ac2be5acc
Mahesh <maheshs.manu1993@gmail.com>
2018-06-28 12:24:03 +0530

    file-trx

--- temp_file.php
+++ temp_file.php
@@ -1,3 +1,77 @@
-<?php
-//Tepm file here
-?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>btclanding</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/css/swiper.min.css">
+    <link rel="stylesheet" href="https://daneden.github.io/animate.css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lity/1.2.0/lity.min.js"></script>
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lity/1.2.0/lity.min.css">
+<script type="text/javascript">
+                    function googleTranslateElementInit() {
+                        new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+                    }
+                </script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+</head>
+
+<body style="font-family: Montserrat">
+    <div style="text-align:center;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Transactions</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div style="text-align:center;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th style="width:80px;">S.NO</th>
+                                    <th style="width:170px;">Date</th>
+                                    <th>From</th>
+                                    <th style="width:200px;">Amount</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>16-06-2018 &nbsp;15:36</td>
+                                    <td>XXXXXinDH67DfghhhtdcdcmjuydsdVUIvide3lk4MjU3XXXXXX</td>
+                                    <td>0.3000000 XMR</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>16-06-2018 &nbsp;16:06<br></td>
+                                    <td>XXXXXinDH67DVyjsvdodrttitdlfshvcHlbTg UIvide3lk4MjU3XXXXXX<br></td>
+                                    <td>2.4670000 XMR</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/script.min.js"></script>
+</body>
+
+</html>
